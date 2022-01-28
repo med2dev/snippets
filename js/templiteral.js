08b80b3a796f3749904bcfab5f8f07057d7a4ee8
med2dev <daniel.bryce.hall@gmail.com>
2022-01-27 23:20:00 -0700

    implement form and button

--- js/templiteral.js
+++ js/templiteral.js
@@ -1,1 +1,19 @@
-document.querySelector('word1')
+function combineWords(){
+    const [first, second] = getWords()
+    let output = `${first} ${second}`
+    document.querySelector('#outputHere').innerText = output
+}
+
+function getWords(){
+    const firstWord = document.querySelector('#firstWord').value 
+    const secondWord = document.querySelector('#secondWord').value
+    return [firstWord, secondWord]
+}
+
+// get inputs from the DOM
+// const firstWord = document.querySelector('#firstWord').value
+// const secondWord = document.querySelector('#secondWord').value
+
+// listen for click
+const buttonElement = document.querySelector('#combineWords')
+buttonElement.addEventListener('click', combineWords)
