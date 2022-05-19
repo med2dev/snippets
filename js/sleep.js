54bd59615b2cc8e2742a06aa9afd52bef0a28d1a
med2dev <daniel.bryce.hall@gmail.com>
2022-05-19 14:18:38 -0700

    minor edit

---
+++ js/sleep.js
@@ -0,0 +1,11 @@
+// sleep function
+
+function sleep(ms){
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+// example
+async function_do_it_later(ms){
+  await sleep(ms)
+  console.log(`You waited ${ms} milliseconds to do this`)
+}
